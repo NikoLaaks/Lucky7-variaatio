316eda1e044be2012e2c12048e3d896268e6d285
NikoLaaks <niko23010@student.hamk.fi>
2023-09-04 10:41:14 +0300

    Added scanner
    Käyttäjä voi nyt päättää itse rahamäärän alussa

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -1,11 +1,14 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lucky7 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		Random rand = new Random();
 		int nro1 = 0;
-		int money = 5;
+		System.out.println("Anna rahamäärä");
+		int money = Integer.parseInt(scanner.nextLine());
 		
 		while(money > 0) {
 			money--;
