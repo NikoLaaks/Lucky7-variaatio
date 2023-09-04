e9dafca7c2fc7a4b1227c283acfbda34abc12f5c
NikoLaaks <niko23010@student.hamk.fi>
2023-09-04 19:06:09 +0300

    Puuttuvien ominaisuuksien lisäys

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -7,18 +7,33 @@ public class Lucky7 {
 		Scanner scanner = new Scanner(System.in);
 		Random rand = new Random();
 		int nro1 = 0;
+		int nro2 = 0;
+		int nro3 = 0;
 		System.out.println("Anna rahamäärä");
 		int money = Integer.parseInt(scanner.nextLine());
 		
 		while(money > 0) {
 			money--;
 			nro1 = rand.nextInt(10) + 1;
-			if(nro1 == 7) {
+			nro2 = rand.nextInt(10) + 1;
+			nro3 = rand.nextInt(10) + 1;
+			if(nro1 == 7 && nro2 == 7 && nro3 == 7) {
+				money = money + 10;
+				System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 10 rahaa" );
+				System.out.println("Rahaa jäljellä " + money);
+			}
+			else if(nro1 == 7 && nro2 == 7 || nro2 == 7 && nro3 == 7 || nro1 == 7 && nro3 == 7) {
+				money = money + 5;
+				System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 5 rahaa");
+				System.out.println("Rahaa jäljellä " + money);
+			}
+			else if(nro1 == 7 || nro2 == 7 || nro3 == 7) {
 				money = money + 3;
-				System.out.println("Sait numeron " + nro1 + ". Voitit pelin");
+				System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Voitit pelin ja sait 3 rahaa");
+				System.out.println("Rahaa jäljellä " + money);
 			}else {
-				System.out.println("Sait numeron " + nro1 + ". Hävisit pelin");
-				
+				System.out.println("Sait numerot " + nro1 + ", " + nro2 + " ja " + nro3 + ". Hävisit pelin");
+				System.out.println("Rahaa jäljellä " + money);				
 			}
 			System.out.println("Haluatko pelata uudestaan?");
 			System.out.println("Jos haluat pelata, paina Enter");
