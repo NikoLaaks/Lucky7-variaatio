a9f042a36a41589ecd9bea8771013d5e964706a4
NikoLaaks <niko23010@student.hamk.fi>
2023-09-04 10:33:09 +0300

    Eka committi

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -1,9 +1,19 @@
+import java.util.Random;
 
 public class Lucky7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Random rand = new Random();
+		int nro1 = 0;
+		for(int i = 0; i < 3; i++) {
+			nro1 = rand.nextInt(7) + 1;
+			if(nro1 == 7) {
+				System.out.println("Voitit pelin");
+			}else {
+				System.out.println("Hävisit pelin");
+				
+			}
+		}
 	}
 
 }
