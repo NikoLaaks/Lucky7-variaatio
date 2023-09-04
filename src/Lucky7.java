e9a35178639a53b2b50cd8e92378e7f466ec29c1
NikoLaaks <niko23010@student.hamk.fi>
2023-09-04 10:36:26 +0300

    Toinen committi

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -5,9 +5,13 @@ public class Lucky7 {
 	public static void main(String[] args) {
 		Random rand = new Random();
 		int nro1 = 0;
-		for(int i = 0; i < 3; i++) {
+		int money = 5;
+		
+		while(money > 0) {
+			money--;
 			nro1 = rand.nextInt(7) + 1;
 			if(nro1 == 7) {
+				money = money + 3;
 				System.out.println("Voitit pelin");
 			}else {
 				System.out.println("Hävisit pelin");
